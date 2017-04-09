96a3aa0951cedea65f096ead52158befb7e8193e
arjun <arjuntalpallikar@utexas.edu>
2017-04-09 15:01:15 -0500

    Added more information
    Added some stuff

--- explorer/views.py
+++ explorer/views.py
@@ -3,6 +3,11 @@ from django.utils import timezone
 from .models import Post
 
 # Create your views here.
-def leg_list(request):
-    Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'explorer/leg_list.html', {'posts': posts})
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'explorer/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'explorer/post_detail.html', {'post': post})
+
