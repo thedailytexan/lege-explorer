b747d05159f50fd3721d92d0b633ff3a694f8d63
arjun <arjuntalpallikar@utexas.edu>
2017-04-10 18:41:04 -0500

    revert to 7105c0b4839deb01c2420d64386b49460aeb15f5

--- explorer/views.py
+++ explorer/views.py
@@ -8,9 +8,7 @@ from django.shortcuts import render, get_object_or_404
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'explorer/post_list.html', {'posts': posts})
-def homepage(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'explorer/homepage.html', {'posts': posts})
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'explorer/post_detail.html', {'post': post})
